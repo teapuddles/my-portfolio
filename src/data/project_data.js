6ccd2ba5fa5ef9b39134592a530aedb68ea00647
teapuddles <kingsley.gleeson@gmail.com>
2021-06-02 21:23:53 -0400

    added more data

--- src/data/project_data.js
+++ src/data/project_data.js
@@ -1,27 +1,56 @@
 export const projects = [
     {
-        title: "Bloo Karaoke",
-        hostLink: "heroku link here",
-        githubLink: "github link here",
-            Description: "description of Bloo Karaoke ", 
-        techUsed: [{
-            detail1: "React",
-            detail2: "npm-react-piano",
-            detail3: "stuff",
-            detail4: "things",
-        }],
-    },
-    {
         title: "Covid-19 Tracker",
         hostLink: "heroku link here",
         githubLink: "github link here",
             Description: "description of Covid Tracker ", 
         techUsed: [{
-            detail1: "React",
+            detail1: "React.js",
             detail2: "Redux",
-            detail3: "Styled Components",
-            detail4: "things",
-            detail5: ""
+            detail3: "Ruby on Rails",
+            detail4: "PostgreSQL",
+            detail5: "Styled Components",
+            detail6: "NewsAPI",
+            detail7: "Disease.sh",
+            detail8: "Heroku One-off Dynos"
+        }],
+    },
+    {
+        title: "Bloo Karaoke",
+        hostLink: "heroku link here",
+        githubLink: "github link here",
+            Description: "description of Bloo Karaoke ", 
+        techUsed: [{
+            detail1: "React.js",
+            detail2: "Ruby on Rails",
+            detail3: "npm-react-piano",
+            detail4: "PostgreSQL",
+            detail5: "Custom CSS"
+        }],
+    },
+    {
+        title: "Fifteen Minute Food",
+        hostLink: "heroku link here",
+        githubLink: "github link here",
+            Description: "description of Fifteen Minute Food ", 
+        techUsed: [{
+            detail1: "React.js",
+            detail2: "Ruby on Rails",
+            detail3: "PostgreSQL",
+            detail4: "Yelp API",
+            detail5: "Stripe API",
+            detail5: "Custom CSS",
+        }],
+    },
+    {
+        title: "This Portfolio",
+        hostLink: "heroku link here",
+        githubLink: "github link here",
+            Description: "description of This Portfolio ", 
+        techUsed: [{
+            detail1: "Gatsby",
+            detail2: "React Components",
+            detail3: "Css Stuff",
         }],
     }
 ]
