40e7e28b781e296f7a39b309ee8958051893f621
teapuddles <kingsley.gleeson@gmail.com>
2021-06-13 14:20:22 -0400

    added gatsby image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,10 @@ module.exports = {
   siteMetadata: {
     title: "Kevin's Portfolio",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+  "gatsby-plugin-gatsby-cloud",
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",
+],
+  
 };
--- src/components/ResumeComponent.js
+++ src/components/ResumeComponent.js
@@ -1,10 +1,19 @@
 import React from 'react'
+// import * as ResumePDF from '../images/Resume_Tech_2021'
 
 const ResumeComponent = () => {
 
     return(
         <div className="pdfContainer" >
-            HI THERE
+            {/* <object
+            data={ResumePDF}
+            type='application.pdf'
+            title='Kevin Gleeson Resume 2021'
+            aria-label="Kevin Gleeson Tech Resume"
+            style={{ border: '1', color: 'black' }}
+            width='670'
+            height='600'
+          /> */}
         </div>
     )
 }
