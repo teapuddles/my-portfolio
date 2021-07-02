f9102144226a0320cb1345ca8fabf1b0310ea374
teapuddles <kingsley.gleeson@gmail.com>
2021-07-02 14:29:32 -0400

    put medium posts at bottom of main, need blue-gray color for divs

--- src/components/StyledProjectComponent.js
+++ src/components/StyledProjectComponent.js
@@ -4,11 +4,15 @@ import styled from "styled-components";
 export const ProjectSlice = styled.div`
     padding: 10px; 
     margin: 20px;
-    border: solid 1px red;
+    height: 10rem;
+    background-color: #DEDDD9;
+    border-radius: 5px;
 `
 
 export const ProjectSliceWrapper = styled.div`
-    border: dotted 5px orange;
+    box-shadow: 5px 10px 5px #302F31;
+    background-color: #B1B5BB;
+    border-radius: 5px;
     margin: 20px;
     height: 200px;
 `
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import Layout from '../components/Layout.js'
-import ProjectList from '../components/ProjectList'
-import { ProjectSliceContainer } from '../components/StyledProjectComponent'
+import React from 'react';
+import Layout from '../components/Layout.js';
+import ProjectList from '../components/ProjectList';
+import { ProjectSliceContainer } from '../components/StyledProjectComponent';
 
-import { projects } from '../data/project_data'
+import { projects } from '../data/project_data';
 
 // my goal here is to have this projects page, which yeilds our project list. 
 // When a user clicks on a list item, a modal pops out with information on that project.
@@ -14,9 +14,9 @@ const Projects = () => {
     return(
         <Layout pageTitle="Kevin's Projects">
         <ProjectSliceContainer>
-        {projects.map((singleProject) => (
-            <ProjectList key={singleProject.title} singleProject={singleProject} />
-          ))}
+            {projects.map((singleProject) => (
+             <ProjectList key={singleProject.title} singleProject={singleProject} />
+            ))}
         </ProjectSliceContainer>
         </Layout>
     )
