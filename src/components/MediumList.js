308405ea4786a5771267f6e9638b89f33dfb800f
teapuddles <kingsley.gleeson@gmail.com>
2021-07-11 14:13:50 -0400

    medium posts link to medium page

--- src/components/MediumList.js
+++ src/components/MediumList.js
@@ -3,10 +3,11 @@ import React from 'react'
 const MediumList = (props) => {
 
     const postInfo = props.post.node
+    console.log(postInfo)
 
     return(
         <div className="medium-slice" >
-            <div className='post-title'>{postInfo.title}</div>
+            <a className='post-title' href={`https://medium.com/@teapuddles/${postInfo.uniqueSlug}`}>{postInfo.title}</a>
             <div className='post-content'>{postInfo.content.subtitle}</div>
         </div>
     )
