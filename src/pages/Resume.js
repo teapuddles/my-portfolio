0d08ab90c7b9427803538ca1bce9596dbbfeff8a
teapuddles <kingsley.gleeson@gmail.com>
2021-07-22 00:44:39 -0400

    clouds added everywhere

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,13 +2,23 @@ import React from 'react'
 import ResumeComponent from '../components/ResumeComponent';
 import Layout from '../components/Layout.js';
 
+import smallCloudFlip from '../images/kevinPics/smallCloudFlip.png'
+
 
 const Resume = () => {
 
     return(
         <Layout pageTitle='Current Resume'>
-            <div className="Resume-section">
+            <div className='resume-section'>
                     <ResumeComponent />
+            <div className="resume-cloud-wrapper">
+            <img key={smallCloudFlip} 
+             src={smallCloudFlip} 
+             alt={'small cloud'} 
+             className='resume-cloud' 
+            >
+            </img>
+            </div>
             </div>
         </Layout>
     )
